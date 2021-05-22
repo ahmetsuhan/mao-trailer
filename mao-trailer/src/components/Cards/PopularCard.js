f6dcca09c3f794c02d354b7e3b6baa5ba83f3407
Ahmet Suhan Oka <ahmet.oka@yemeksepeti.com>
2021-05-22 15:57:12 +0300

    add popularCard component

---
+++ mao-trailer/src/components/Cards/PopularCard.js
@@ -0,0 +1,36 @@
+import React from "react";
+import { useHistory } from "react-router-dom";
+import "../../styles/PopularCard.style.scss";
+import applicationHelper from "../../helpers/applicationHelper";
+const PopularCard = ({ image, year, name = "", point, id }) => {
+  const history = useHistory();
+
+  let cardPoint = parseFloat(point).toString().split(".");
+
+  const handleName = applicationHelper.addSuffixToString;
+
+  const handleClick = (e) => {
+    history.push(`/movies/popularlist/detail/${id}`);
+  };
+
+  return (
+    <div onClick={(e) => handleClick(e)} className="popular-movie-card">
+      <div className="movie-card-img">
+        <img src={image} alt="" />
+      </div>
+
+      <p className="movie-point">
+        {cardPoint[0]}
+        <sup>
+          <small>.{cardPoint[1]}</small>
+        </sup>
+      </p>
+      <div className="footer">
+        <span>{year.split(" ")[0]}</span>
+        <p>{handleName(name, 14, "...")}</p>
+      </div>
+    </div>
+  );
+};
+
+export default React.memo(PopularCard);
